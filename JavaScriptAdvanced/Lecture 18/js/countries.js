3a7cf01b0687a6aebfd72d619431c3d2baaf001b
Amit Balteriski <amitbalti@gmail.com>
2023-01-31 13:52:25 +0200

    new updates

--- JavaScriptAdvanced/Lecture 18/js/countries.js
+++ JavaScriptAdvanced/Lecture 18/js/countries.js
@@ -37,15 +37,25 @@ $(function () {
 const getData = (total) => {
   $("#container").html("");
   for (let i = 0; i < total; i++) {
-    const randomNumber = Math.floor(Math.random() * allCountries.length);
+    // const randomNumber = Math.floor(Math.random() * allCountries.length);
     //  append - will add to the existing
+    //     $("#container").append(`
+    //                   <div class="Box">
+    //                       <img src=${allCountries[randomNumber].flags.png} width=100 height=70/> <br />
+    //                        ${allCountries[randomNumber].capital} <br />
+    //                        ${allCountries[randomNumber].name.common} <br />
+    //                        ${allCountries[randomNumber].population} <br />
+    //                        ${allCountries[randomNumber].continents} <br />
+    //                     </div>
+    //                   `);
+    //   }
     $("#container").append(`
                   <div class="Box">
-                      <img src=${allCountries[randomNumber].flags.png} width=100 height=70/> <br />
-                       ${allCountries[randomNumber].capital} <br />
-                       ${allCountries[randomNumber].name.common} <br />
-                       ${allCountries[randomNumber].population} <br />
-                       ${allCountries[randomNumber].continents} <br />
+                      <img src=${allCountries[i].flags.png} width=100 height=70/> <br />
+                       ${allCountries[i].capital} <br />
+                       ${allCountries[i].name.common} <br />
+                       ${allCountries[i].population} <br />
+                       ${allCountries[i].continents} <br />
                     </div>
                   `);
   }
