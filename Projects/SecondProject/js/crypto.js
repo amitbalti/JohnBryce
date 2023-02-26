dd7a9aba60bdd0eb92fb7798026b76e7b1d460bd
Amit Balteriski <amitbalti@gmail.com>
2023-02-26 21:09:56 +0200

    project2 - update

--- Projects/SecondProject/js/crypto.js
+++ Projects/SecondProject/js/crypto.js
@@ -19,23 +19,81 @@ const getCoinsData = (data) => {
   //   let symbols = "";
   //   let names = "";
   // Names:
+
   $("#allCoins").html("");
   //   for (let i = 0; i < 100; i++) {
-  for (let i = 0; i < 1; i++) {
+  for (let i = 0; i < 6; i++) {
     const randomIndex = Math.floor(Math.random() * data.length);
     let coin = data[randomIndex];
 
     $("#allCoins").append(
-      `<div>
-            <h2>${limit(coin.symbol, 5)}</h2>
-            <h5>${limit(coin.name, 10)}</h5>
-            <button type="button" class="btn btn-primary moreInfoBtn">More Info</button>
-      </div>`
+      `<div class="custom-control custom-switch switchBtn">
+                <label class="switch">
+                <input type="checkbox" />
+                    <span class="slider round"></span>
+                </label>
+                <h2>${limit(coin.symbol, 5)}</h2>
+                <h5>${limit(coin.name, 10)}</h5>
+                <button type="button" class="btn btn-primary moreInfoBtn">
+                    More Info
+                </button>
+        </div>`
     );
   }
+  // How many coins can you select?
+  const howMany = () => {
+    let maxSelected = 5;
+    $(".switch .slider .round").on("click", function () {
+      let count = $(
+        ".switch .slider .round input[type='checkbox']:checked"
+      ).length;
+
+      if (count > maxSelected) {
+        $(this).prop("checked", false);
+        // alert("You can't check more than 5 coins");
+        var selectedCheckboxes = $(
+          '#checkbox-container input[type="checkbox"]:checked'
+        );
+        var checkboxNames = selectedCheckboxes
+          .map(function () {
+            return $(this).attr("name");
+          })
+          .get();
+        var msg =
+          "You can only select " +
+          maxSelected +
+          " options. Please select which option you want to keep:\n\n";
+        for (var i = 0; i < checkboxNames.length; i++) {
+          msg += checkboxNames[i] + "\n";
+        }
+        var selectedCheckboxName = prompt(msg);
+        if (selectedCheckboxName) {
+          selectedCheckboxes.each(function () {
+            if ($(this).attr("name") !== selectedCheckboxName) {
+              $(this).prop("checked", false);
+            }
+          });
+        }
+      }
+    });
+  };
+  //     let totalChecked = 0;
+  //     for (let i = 0; i < coin.length; i++) {
+  //       if (coin.selected) {
+  //         totalChecked++;
+  //       }
+  //       if (totalChecked > maxSelected) {
+  //         alert("You can't check more than 5 coins");
+  //         return false;
+  //       }
+  //       return true;
+  //     }
+  //   });
+  // };
 };
 
 $(async function () {
   let data = await getAllCoins();
   getCoinsData(data);
+  //   howMany();
 });
