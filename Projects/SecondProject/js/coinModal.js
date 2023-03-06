8f21a4de553fb76ba4d7d175711175c63100a987
Amit Balteriski <amitbalti@gmail.com>
2023-03-06 16:33:41 +0200

    second project updates

--- Projects/SecondProject/js/coinModal.js
+++
@@ -1,9 +0,0 @@
-// const coinModal = () => {
-//   let selectedCoins = checkedCoins.slice(0, 5);
-//   $(".container").html(
-//     `
-//         <div class="modal fade modal-lg" id=">
-//         </div>
-//     `
-//   );
-// };
--- Projects/SecondProject/js/crypto.js
+++ Projects/SecondProject/js/crypto.js
@@ -2,15 +2,31 @@ const coinsUrl = "https://api.coingecko.com/api/v3/coins/list";
 const coinsRateUrl = "https://api.coingecko.com/api/v3/coins";
 
 async function getAllCoins() {
-  return await $.get(coinsUrl);
+  $("#progress-bar").show(); // show progress bar
+  return await $.get(coinsUrl, {
+    beforeSend: function () {
+      $("#progress-bar").show();
+    },
+    complete: function () {
+      $("#progress-bar").hide();
+    },
+  });
 }
 
 async function getAllCoinsRate() {
-  return await $.get(coinsRateUrl);
+  $("#progress-bar").show(); // show progress bar
+  return await $.get(coinsRateUrl, {
+    beforeSend: function () {
+      $("#progress-bar").show();
+    },
+    complete: function () {
+      $("#progress-bar").hide();
+    },
+  });
 }
 
-// async function findCoins(name) {
-//   return await $.get(coinsUrl + name);
+// async function findCoins(coinName) {
+//   return await $.get(coinsUrl + coinName);
 // }
 
 const getCoinsData = async (data) => {
@@ -23,26 +39,6 @@ const getCoinsData = async (data) => {
   //   for (let i = 0; i < 100; i++) {
   for (let i = 0; i < 6; i++) {
     let coin = data[i];
-
-    /* Trying to make the data of the info button to show USD, EUR, and ILS current rates */
-    // let market_data = {};
-
-    // if (coin.market_data) {
-    //   Object.keys(coin.market_data).forEach((price) => {
-    //     market_data[price] = (market_data[price] || 0) + 1;
-    //   });
-    // }
-
-    // $("#currentPrice").html("");
-    // Object.keys(market_data).forEach((price) => {
-    //   $("#currentPrice").append(`
-    //                            <h5>${price.usd}</h5>
-    //                            <h5>${price.eur}</h5>
-    //                            <h5>${price.ils}</h5>
-    //                      `);
-    // });
-
-    console.log(parseInt(coin.data_market.current_price.usd));
 
     $("#allCoins").append(
       `<div class="custom-control custom-switch switchBtn">
@@ -57,8 +53,8 @@ const getCoinsData = async (data) => {
                 <button type="button" class="btn btn-primary moreInfoBtn" data-toggle="collapse" data-target="#collapseExample_${i}" aria-expanded="false" aria-controls="collapseExample_${i}">
                     More Info
                 </button>
-                <div class="collapse" id="collapseExample_${i}">
-                    <div class="card card-body" id="currentPrice_${i}">
+                <div class="collapse border-light" id="collapseExample_${i}">
+                    <div class="card card-body border-info" id="currentPrice_${i}">
                     </div>
                 </div>
         </div>`
@@ -70,12 +66,20 @@ const getCoinsData = async (data) => {
 
     // Append current coin rate to the corresponding "currentPrice" div
     $(`#currentPrice_${i}`).append(`
-      <p>Current Rate:</p>
-      <ul>
-        <li>USD: ${coinRates.market_data.current_price.usd}</li>
-        <li>EUR: ${coinRates.market_data.current_price.eur}</li>
-        <li>ILS: ${coinRates.market_data.current_price.ils}</li>
-      </ul>
+    
+        <p class="card-title">Current <span class="infoBtnRate">${limit(
+          coin.symbol,
+          5
+        )}</span> Rate:</p>
+        
+        <h5><span class="infoBtnRate">USD: </span> ${
+          coinRates.market_data.current_price.usd
+        }$</h5>
+        <h5><span class="infoBtnRate">EUR: </span>
+         ${coinRates.market_data.current_price.eur}€</h5>
+        <h5><span class="infoBtnRate">ILS: </span> ${
+          coinRates.market_data.current_price.ils
+        }₪</h5>
     `);
   }
   howMany();
@@ -94,7 +98,7 @@ const howMany = () => {
       console.log("Here");
       $(".modal-body").html(`
       <span class="modalStyle">Only 5 coins can be selected!</span><br/>
-      Please select the coin to be removed: <br/><br/>
+      Please select the coin to be <span class="removed">removed</span>: <br/><br/>
          <select id="removedCoin">
            <option>${checkedIds[0]}</option>
            <option>${checkedIds[1]}</option>
@@ -104,84 +108,18 @@ const howMany = () => {
            <option>${checkedIds[5]}</option>
          </select>
          `);
-      $("#coinModalLabel").modal({ backdrop: "static", keyboard: false });
+      $("#coinModal").modal({ backdrop: "static", keyboard: false });
     }
   });
 };
 
 const removeCoin = () => {
-  //   console.log("clicked");
-  //   console.log($("#removedCoin").val());
+  console.log("clicked");
+  console.log($("#removedCoin").val());
   let removedId = $("#removedCoin").val();
   $("#" + removedId).prop("checked", false);
-  $("#coinModalLabel").modal("hide");
+  $("#coinModal").modal("hide");
 };
-
-//   let selected = $('input[type="checkbox"]');
-//   selected.change(function () {
-//     let checked = selected.filter(":checked");
-//     console.log(checked.length);
-
-//     if (checked.length > maxOptions) {
-//       console.log(checked);
-//       checked.each(() => {
-// console.log($(this)[0].id);
-
-// console.log(checkedIds);
-// $(".modal-body").html(`
-// <select>
-//   <option>${element.id[0]}</option>
-//   <option>${element.id[1]}</option>
-//   <option>${element.id[2]}</option>
-//   <option>${element.id[3]}</option>
-//   <option>${element.id[4]}</option>
-// </select>
-// `);
-//   });
-// };
-//   selected.not(":checked").prop("disabled", true);
-//   $("#maxOptions").val(maxOptions);
-//   let coinModalBody = $("<div>");
-//   checked.each(function (index, element) {
-//     if (index >= maxOptions) {
-//       $(element).prop("checked", false);
-//       coinModalBody.append($("<p>").text($(element).val()));
-//     }
-//   });
-//   let coinModal = $("<div>").addClass("modal fade").appendTo($("body"));
-//   let coinModalDialog = $("<div>")
-//     .addClass("modal-dialog")
-//     .appendTo($(coinModal));
-//   let coinModalContent = $("<div>")
-//     .addClass("modal-content")
-//     .appendTo(coinModalDialog);
-//   let coinModalHeader = $("<div>")
-//     .addClass("modal-header")
-//     .appendTo(coinModalContent);
-//   let coinModalTitle = $("<h5>")
-//     .addClass("modal-title")
-//     .text("Too many options selected")
-//     .appendTo(coinModalHeader);
-//   let coinModalCloseButton = $("<button>")
-//     .addClass("close")
-//     .attr("data-dismiss, 'modal'")
-//     .html("&times;")
-//     .appendTo(coinModalHeader);
-//   let coinModalBodyDiv = $("<div>")
-//     .addClass("modal-body")
-//     .append($("<p>").text("Please unselect some options:"))
-//     .appendTo(coinModalContent);
-//   coinModalBody.appendTo(coinModalBodyDiv);
-//   coinModal.modal("show");
-// } else {
-//   selected.not(":checked").prop("disabled", false);
-//   $("#maxOptions").val("");
-// }
-// $("#optionCountValue").text(maxOptions - checked.length);
-//   });
-// };
-
-// setTimeout(() => {}, 5000);
 
 $(async function () {
   let data = await getAllCoins();
