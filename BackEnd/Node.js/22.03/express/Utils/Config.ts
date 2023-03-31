83da7c4a7fb952da928af3ac395d17342b7f75b1
Amit Balteriski <amitbalti@gmail.com>
2023-03-31 16:40:52 +0300

    backend 29/03/23

--- BackEnd/Node.js/22.03/express/Utils/Config.ts
+++ BackEnd/Node.js/22.03/express/Utils/Config.ts
@@ -1,6 +1,10 @@
 class Config {
   public WebPort = 8080;
-  public dbHost = "db.thouchwand.com";
+  // public dbHost = "db.thouchwand.com";
+  public mySQLhost = "localhost";
+  public mySQLuser = "root";
+  public mySQLpassword = "12345678";
+  public mySQLdatabase = "youtube";
 }
 
 const config = new Config();
---
+++ BackEnd/Node.js/22.03/express/Utils/dal_mysql.ts
@@ -0,0 +1,35 @@
+// dal = abstract data layer
+import mysql from "mysql";
+import config from "./Config";
+
+// creating a connection object
+const connection = mysql.createPool({
+  host: config.mySQLhost,
+  user: config.mySQLuser,
+  password: config.mySQLpassword,
+  database: config.mySQLdatabase,
+});
+
+console.log("We are connected to mysql");
+
+function execute(sql: string): Promise<any> {
+  return new Promise<any>((resolve, reject) => {
+    // connecction and execute the sql command
+    connection.query(sql, (err, res) => {
+      // if there is an error (sql server doesn't understand the query)
+      if (err) {
+        reject(err);
+        return;
+      }
+      // no error - report data
+      resolve(res);
+    });
+  });
+}
+
+export default {
+  execute,
+};
+
+// promise have 3 types:
+// pending, fullfiled, rejected.
