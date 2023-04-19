a94abe280d09fcd7d9102a26a63c86a48d53cf4b
Amit Balteriski <amitbalti@gmail.com>
2023-04-19 12:21:04 +0300

    update

--- FrontEnd/React/20.03/my_songs/frontend/src/Components/example/getSongData/getSongData.tsx
+++
@@ -1,9 +0,0 @@
-import "./getSongData.css";
-
-function GetSongData(): JSX.Element {
-  // Song information: loading, data, error
-
-  return <div className="getSongData"></div>;
-}
-
-export default GetSongData;
