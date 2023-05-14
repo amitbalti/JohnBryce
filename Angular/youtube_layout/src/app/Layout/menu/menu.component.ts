6441391a457a21366c82a04d6cb06c7e07d39c7c
Amit Balteriski <amitbalti@gmail.com>
2023-05-14 12:16:20 +0300

    angular update

--- Angular/youtube_layout/src/app/Layout/menu/menu.component.ts
+++ Angular/youtube_layout/src/app/Layout/menu/menu.component.ts
@@ -2,9 +2,40 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
+  template: `
+    <div ng-app="myApp" ng-controller="myCtrl">
+      <select
+        [(ngModel)]="selectedColor"
+        (ngModelChange)="changeColor()"
+        name="colors"
+        id=""
+      >
+        <option value="" disabled selected>Choose the color</option>
+        <option value="green">Green</option>
+        <option value="blue">Blue</option>
+        <option value="orange">Orange</option>
+        <option value="red">Red</option>
+        <option value="yellow">Yellow</option>
+        <option value="deeppink">Pink</option>
+        <option value="black">Black</option>
+        <option value="white">White</option>
+      </select>
+      <h2 [style.color]="selectedColor">The color changed!</h2>
+    </div>
+  `,
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.css'],
 })
 export class MenuComponent {
+  selectedColor: string = 'white';
 
+  changeColor() {
+    // Do nothing if no color is selected
+    if (!this.selectedColor) {
+      return;
+    }
+    // Change the color of the h2 tag based on the selected color
+    const h2 = document.getElementsByTagName('h2')[0];
+    h2.style.color = this.selectedColor;
+  }
 }
