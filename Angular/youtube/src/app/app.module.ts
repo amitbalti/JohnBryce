d9690daa09c1a83784521b4f5359d7c520b08df5
Amit Balteriski <amitbalti@gmail.com>
2023-05-28 18:45:53 +0300

    youtube player

--- Angular/youtube/src/app/app.module.ts
+++ Angular/youtube/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { YouTubePlayerModule } from '@angular/youtube-player';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -28,13 +29,11 @@ import { MainPageComponent } from './Pages/main-page/main-page.component';
     CategoriesComponent,
     AboutComponent,
     PlayerComponent,
-    MainPageComponent
+    MainPageComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, YouTubePlayerModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
+let apiLoaded = false;
