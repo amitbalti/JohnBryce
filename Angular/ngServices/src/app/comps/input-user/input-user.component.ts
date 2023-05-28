3193bb7140fccb4c425621231707b6108c16cfe7
Amit Balteriski <amitbalti@gmail.com>
2023-05-28 11:48:15 +0300

    targil 1 17/05

--- Angular/ngServices/src/app/comps/input-user/input-user.component.ts
+++ Angular/ngServices/src/app/comps/input-user/input-user.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { FruitService } from './../../services/fruit.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-input-user',
   templateUrl: './input-user.component.html',
   styleUrls: ['./input-user.component.css'],
 })
-export class InputUserComponent {
+export class InputUserComponent implements OnInit {
   fruit_input = '';
+
+  constructor(private fruitService: FruitService) {}
+
+  ngOnInit() {}
+
+  addFruit() {
+    this.fruitService.addFruit(this.fruit_input);
+  }
 }
